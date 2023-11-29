fb80738c3ec78e6b2a534dc5e0fe81b3ab539870
BhanuGorantla <143116800+BhanuGorantla@users.noreply.github.com>
2023-11-28 22:40:40 -0600

    doneeeeeees till 13

--- models/bike.js
+++ models/bike.js
@@ -1,5 +1,19 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-const bikeSchema = mongoose.Schema({name: String,color: String,price: Number});
+const carSchema = mongoose.Schema({
+  brand: {
+    type: String,
+    required: true,
+    minlength: [2, "Brand name must be at least 2 characters long"],
+    maxlength: [50, "Brand name must not exceed 50 characters"],
+  },
+  color: String,
+  price: {
+    type: Number,
+    required: true,
+    min: [0, "Price must be a non-negative value"],
+    max: [100000, "Price must be less than or equal to 100000"],
+  },
+});
 
-module.exports = mongoose.model("bike",bikeSchema);
+module.exports = mongoose.model("car", carSchema);
--- routes/bikes.js
+++ routes/bikes.js
@@ -1,17 +1,29 @@
 var express = require('express');
 const bike_controlers= require('../controllers/bike');
 var router = express.Router();
+const secured = (req, res, next) => {
+    if (req.user){
+    return next();
+    }
+    res.redirect("/login");
+    }
+
 /* GET costumes */
 router.get('/', bike_controlers.bike_view_all_Page );
-module.exports = router;
+
 
 /* GET detail costume page */
 router.get('/detail', bike_controlers.bike_view_one_Page);
 
 /* GET create costume page */
-router.get('/create', bike_controlers.bike_create_Page);
+router.get('/create',secured, bike_controlers.bike_create_Page);
 
-router.get('/update', bike_controlers.bike_update_Page);
+router.get('/update',secured, bike_controlers.bike_update_Page);
 
 /* GET delete costume page */
-router.get('/delete', bike_controlers.bike_delete_Page);
+router.get('/delete',secured, bike_controlers.bike_delete_Page);
+
+
+
+
+ module.exports = router;
